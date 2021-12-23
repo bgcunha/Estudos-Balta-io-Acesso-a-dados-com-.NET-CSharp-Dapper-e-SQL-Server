0df1e862aebdf1e42f01f21aa42edcd8bb8b50e9
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-23 16:00:37 -0300

    Projeto Blog

---
+++ Blog/Program.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Blog
+{
+    class Program
+    {
+        private readonly static string CONNECTION_STRING = "Server=DESKTOP-BKLEGIN\\SQLEXPRESS2019;Database=Blog;User ID=sa;Password=sa123";
+
+        static void Main(string[] args)
+        {
+            ReadUsers();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                 foreach (var item in users)
+                 Console.WriteLine(item.Email);
+            }
+
+            // var users = repository.Read();
+            
+        }
+    }
+}
