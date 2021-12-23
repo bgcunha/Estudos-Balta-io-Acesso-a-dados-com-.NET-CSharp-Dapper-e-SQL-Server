1aaf247af213b494211c9dc389d0b0c262dad633
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-23 16:12:28 -0300

    CRUD

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -11,9 +11,7 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            ReadUsers();
-
-            Console.WriteLine("Hello World!");
+            DeleteUser();
         }
 
         private static void ReadUsers()
@@ -22,12 +20,72 @@ namespace Blog
             {
                 var users = connection.GetAll<User>();
 
-                 foreach (var item in users)
-                 Console.WriteLine(item.Email);
+                foreach (var item in users)
+                    Console.WriteLine(item.Email);
             }
 
-            // var users = repository.Read();
-            
+        }
+        private static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Email);
+            }
+        }
+
+        private static void CreateUser()
+        {
+            var user = new User
+            {
+                Bio = "Equipe balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var rows = connection.Insert<User>(user);
+
+                Console.WriteLine("Cadastro Realizado com sucesso!");
+            }
+
+        }
+
+        private static void UpdateUser()
+        {
+            var user = new User
+            {
+                Id = 2,
+                Bio = "Equipe | balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe de suporte balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var rows = connection.Update<User>(user);
+
+                Console.WriteLine("Cadastro Atualizado com sucesso!");
+            }
+        }
+
+        private static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                connection.Delete<User>(user);
+
+                Console.WriteLine("Deletado com sucesso!");
+            }
         }
     }
 }
