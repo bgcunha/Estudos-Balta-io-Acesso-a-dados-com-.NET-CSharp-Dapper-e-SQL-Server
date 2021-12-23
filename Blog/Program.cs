6c8436f1ee7bb8d2fcd847233cf45809bfc4505b
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-23 17:27:17 -0300

    Alterando para utilizar repositorio generico

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -12,97 +12,139 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            // CreateUser(repository);
-            // UpdateUser(repository);
-            // DeleteUser(repository);
-            // ReadUser(repository);
-            ReadUsers(new SqlConnection(CONNECTION_STRING));
-            ReadRoles(new SqlConnection(CONNECTION_STRING));
+            using var connection = new SqlConnection(CONNECTION_STRING);
+
+            DeleteUser(connection);
+            // CreateUser(connection);
+            // UpdateUser(connection);
+            // ReadUsers(connection);
+            // ReadRoles(connection);
+            // ReadTags(connection);
             // ReadWithRoles(connection);
         }
 
         private static void ReadUsers(SqlConnection connection)
         {
             var repository = new Repository<User>(connection);
-            var users = repository.Read();
+            var items = repository.Read();
 
-            foreach (var user in users)
-                Console.WriteLine(user.Name);
-        }
-        private static void ReadUser(SqlConnection connection)
-        {
-            var repository = new Repository<User>(connection);
-            var user = repository.Read(1);
-            Console.WriteLine(user.Name);
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
 
         private static void ReadRoles(SqlConnection connection)
         {
             var repository = new Repository<Role>(connection);
-            var roles = repository.Read();
+            var items = repository.Read();
 
-            foreach (var role in roles)
-                Console.WriteLine(role.Name);
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
-        private static void ReadRole(SqlConnection connection)
+
+        private static void ReadTags(SqlConnection connection)
         {
-            var repository = new Repository<Role>(connection);
-            var role = repository.Read(1);
-            Console.WriteLine(role.Name);
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Read();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
         }
 
-        private static void CreateUser()
+        private static void CreateUser(SqlConnection connection)
         {
             var user = new User
             {
                 Bio = "Equipe balta.io",
-                Email = "hello@balta.io",
+                Email = "equipe@balta.io",
                 Image = "https://...",
                 Name = "Equipe balta.io",
                 PasswordHash = "HASH",
-                Slug = "equipe-balta"
+                Slug = "equipe-balta-io"
             };
 
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var rows = connection.Insert<User>(user);
-
-                Console.WriteLine("Cadastro Realizado com sucesso!");
-            }
-
+            var repository = new Repository<User>(connection);
+            repository.Create(user);
         }
 
-        private static void UpdateUser()
+
+        private static void UpdateUser(SqlConnection connection)
         {
             var user = new User
             {
                 Id = 2,
-                Bio = "Equipe | balta.io",
+                Bio = "Equipe balta.io",
                 Email = "hello@balta.io",
                 Image = "https://...",
-                Name = "Equipe de suporte balta.io",
+                Name = "Equipe de sustentação balta.io",
                 PasswordHash = "HASH",
                 Slug = "equipe-balta"
             };
 
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var rows = connection.Update<User>(user);
-
-                Console.WriteLine("Cadastro Atualizado com sucesso!");
-            }
+            var repository = new Repository<User>(connection);
+            repository.Update(user);
         }
 
-        private static void DeleteUser()
+        private static void DeleteUser(SqlConnection connection)
         {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(1);
+            var repository = new Repository<User>(connection);
+            var item = repository.Read(3);
 
-                connection.Delete<User>(user);
-
-                Console.WriteLine("Deletado com sucesso!");
-            }
+            repository.Delete(item);
         }
+
+
+        // private static void CreateUser()
+        // {
+        //     var user = new User
+        //     {
+        //         Bio = "Equipe balta.io",
+        //         Email = "hello@balta.io",
+        //         Image = "https://...",
+        //         Name = "Equipe balta.io",
+        //         PasswordHash = "HASH",
+        //         Slug = "equipe-balta"
+        //     };
+
+        //     using (var connection = new SqlConnection(CONNECTION_STRING))
+        //     {
+        //         var rows = connection.Insert<User>(user);
+
+        //         Console.WriteLine("Cadastro Realizado com sucesso!");
+        //     }
+
+        // }
+
+        // private static void UpdateUser()
+        // {
+        //     var user = new User
+        //     {
+        //         Id = 2,
+        //         Bio = "Equipe | balta.io",
+        //         Email = "hello@balta.io",
+        //         Image = "https://...",
+        //         Name = "Equipe de suporte balta.io",
+        //         PasswordHash = "HASH",
+        //         Slug = "equipe-balta"
+        //     };
+
+        //     using (var connection = new SqlConnection(CONNECTION_STRING))
+        //     {
+        //         var rows = connection.Update<User>(user);
+
+        //         Console.WriteLine("Cadastro Atualizado com sucesso!");
+        //     }
+        // }
+
+        // private static void DeleteUser()
+        // {
+        //     using (var connection = new SqlConnection(CONNECTION_STRING))
+        //     {
+        //         var user = connection.Get<User>(1);
+
+        //         connection.Delete<User>(user);
+
+        //         Console.WriteLine("Deletado com sucesso!");
+        //     }
+        // }
     }
 }
